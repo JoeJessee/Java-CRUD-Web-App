06f944137e9517adc822044d2d97ee13b46ae2ff
shannoncantech <LdwpEtegOLLon9#1>
2018-08-26 00:37:32 -0400

    Initial commit

---
+++ src/main/java/com/shannoncantech/demo/CompleteDataLifeCyleApplication.java
@@ -0,0 +1,12 @@
+package com.shannoncantech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteDataLifeCyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompleteDataLifeCyleApplication.class, args);
+    }
+}
---
+++ src/main/java/com/shannoncantech/demo/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.shannoncantech.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Long> {
+}
